b132cba709fc31e98df690ed2c68973a39b33988
menard soliven <msolve@msn.com>
2024-01-15 22:27:52 -0500

    enhanced to use line chart

--- src/app/calculators/when-to-retire/when-to-retire.component.ts
+++ src/app/calculators/when-to-retire/when-to-retire.component.ts
@@ -11,13 +11,13 @@ import { BaseComponent } from 'src/app/base/base.component';
 
 class TableDataRow {
   public year: string;
-  public balance: string;
-  public contributions: string;
+  public value1: string;
+  public value2: string;
 
   public constructor(y: string, b: string, c: string) {
     this.year = y;
-    this.balance = b;
-    this.contributions = c;
+    this.value1 = b;
+    this.value2 = c;
   }
 }
 
@@ -57,9 +57,37 @@ export class WhenToRetireComponent extends BaseComponent implements OnInit {
       maintainAspectRatio: false
       };
   }
+  public lineChartOptions(minY: number, maxX: number): ChartConfiguration['options'] {
+    return {
+      responsive: true,
+      // We use these empty structures as placeholders for dynamic theming.
+      scales: {
+          x: {
+            max: maxX,
+            stacked: true
+          },
+          y: {
+            min: minY
+          }
+        },
+        plugins: {
+          legend: {
+            display: true,
+          },
+          datalabels: {
+            // anchor: 'end',
+            // align: 'end',
+            display: false
+          }
+        },
+      maintainAspectRatio: false
+      };
+  }
 
   public barChartType: ChartType = 'bar';
-  public barChartPlugins = [
+  public lineChartType: ChartType = 'line';
+  
+  public chartPlugins = [
     DataLabelsPlugin
   ];
 
@@ -70,9 +98,17 @@ export class WhenToRetireComponent extends BaseComponent implements OnInit {
     ]
   };
 
+  public lineChartData: ChartData<'line'> = {
+    labels: [],
+    datasets: [
+      { data: [], label: 'Growth' }
+    ]
+  };
+
   public tableDataRows: any[] = [];
   public showTable: boolean = false;
   public disableTable: boolean = false;
+  public showAsset: boolean = false;
 
   public daysMode: boolean = false;
 
@@ -103,11 +139,12 @@ export class WhenToRetireComponent extends BaseComponent implements OnInit {
         let initial = params["initial"] as string;
         let contrib = params["contrib"] as string;
         let expenses = params["expenses"] as string;
+        let inflate = params["inflate"] as string;
         let rate = params["rate"] as string;
         let age = params["age"] as string;
         let view = params["view"] as string;
         
-        this.onInitForm(initial, contrib, expenses, rate, age, view);
+        this.onInitForm(initial, contrib, expenses, inflate, rate, age, view);
         this.onChange(true);
       }
     );
@@ -115,7 +152,7 @@ export class WhenToRetireComponent extends BaseComponent implements OnInit {
 
   }
 
- onInitForm(initial: string, contrib: string, expenses: string, rate: string, age: string, view: string) {
+  onInitForm(initial: string, contrib: string, expenses: string, inflate: string, rate: string, age: string, view: string) {
 
     const getValue = (val: string, def: any) => {
       if (!val) return def;
@@ -155,6 +192,7 @@ export class WhenToRetireComponent extends BaseComponent implements OnInit {
       initialPrincipal: [getValue(initial, "$100,000.00"), Validators.required],
       monthlyContribution: [getValue(contrib, "$1000.00")],
       monthlyExpenses: [getValue(expenses, "$5000.00")],
+      inflationRate: [getValue(inflate, "3")],
       investmentRate: [getValue(rate, "8")],
       currentAge: [getValue(age, "30")],
       viewMode: [getValue(view, "")]
@@ -167,24 +205,27 @@ export class WhenToRetireComponent extends BaseComponent implements OnInit {
     let input = this.inputForm.value;
 
     if (!init && input.viewMode) {
-      this.onInitForm(input.initialPrincipal, input.monthlyContribution, input.monthlyExpenses, input.investmentRate,
-        input.currentAge, input.viewMode);
+      this.onInitForm(input.initialPrincipal, input.monthlyContribution, input.monthlyExpenses, input.inflationRate,
+        input.investmentRate, input.currentAge, input.viewMode);
       input = this.inputForm.value;
     }
     
     let principal = FormatUtil.parseToNumber(input.initialPrincipal);
     let contrib = FormatUtil.parseToNumber(input.monthlyContribution);
     let expenses = FormatUtil.parseToNumber(input.monthlyExpenses);
+    let inflate = FormatUtil.parseToNumber(input.inflationRate);
     let rate = FormatUtil.parseToNumber(input.investmentRate);
     let age = FormatUtil.parseToNumber(input.currentAge);
 
     // reset retirement age
     this.retireAge = -1;
 
-    this.barChartData.labels = this.calcLabels(input.currentAge as number);
-    this.barChartData.datasets = this.calcResultsets(principal, contrib, expenses,
-      rate, age, input.viewMode);
-    
+    this.barChartData.labels = this.lineChartData.labels 
+      = this.calcLabels(input.currentAge as number);  
+  
+    this.barChartData.datasets = this.lineChartData.datasets 
+      = this.calcResultsets(principal, contrib, expenses, inflate, rate, age, input.viewMode);
+
     this.chart?.update();
 
     this.showTable = false;
@@ -200,48 +241,67 @@ export class WhenToRetireComponent extends BaseComponent implements OnInit {
     return labels;
   }
 
-  calcResultsets(initialPrincipal: number, regularContribs: number, expenses: number, investmentRate: number, age: number, viewmode?: string): any {
+  calcResultsets(initialPrincipal: number, regularContribs: number, expense: number, inflationRate: number, 
+    investmentRate: number, age: number, viewmode?: string): any {
 
       let balances: number[] = [];
+      let incomes: number[] = [];
       let contributions: number[] = [];
-      let compoundRate = investmentRate / 100;
+      let expenses: number[] = [];
       let balance: number = initialPrincipal;
       let totalContribs: number = initialPrincipal;
       let growth: number;
       let contribs: number = 0;
    
-      let expensePerYear = expenses * 12;
-      let targetPrincipal = expensePerYear * 25;
-   
-      const calcGrowth = (balance: number, compoundRate: number): number => {
-        return balance * compoundRate;
+      const calcRetireIncome = (balance: number): number => {
+        return balance * .04;
       };
 
+      const calcFireNumber = (expensePerYear: number): number => {
+        return expensePerYear * 25;
+      };
+
+      const calcGrowth = (balance: number, compoundRate: number): number => {
+        return balance * compoundRate / 100;
+      };
+
+      let expensePerYear = expense * 12;
+      let targetPrincipal = calcFireNumber(expensePerYear);
+      let potentialIncome: number = calcRetireIncome(initialPrincipal);
+   
       for (let i = 0; i <= 80; i++) {
         if (i == 0) {
           balances.push(balance);
+          incomes.push(potentialIncome);
           contributions.push(totalContribs);
+          expenses.push(expensePerYear);     
         }
         else {
           contribs += regularContribs * 12;
 
-          growth = calcGrowth(balance, compoundRate);
+          growth = calcGrowth(balance, investmentRate);
           balance += growth + contribs;    
+          potentialIncome = calcRetireIncome(balance);
 
           totalContribs += contribs; 
           contribs = 0;        
 
+          
           balances.push(balance);
-          contributions.push(totalContribs);      
+          incomes.push(potentialIncome);
+          contributions.push(totalContribs);
+          expenses.push(expensePerYear);     
 
-          if (this.retireAge == -1 && balance >= targetPrincipal ) {
+          if (this.retireAge == -1 && potentialIncome >= expensePerYear ) {
             this.retireAge = age + i;
             this.retireBalance = FormatUtil.formatNumber(balance, 0);
-            this.retireIncome = FormatUtil.formatNumber(balance * .04, 0);
-            this.monthlyRetireIncome = FormatUtil.formatNumber(balance * .04 / 12, 0);
+            this.retireIncome = FormatUtil.formatNumber(calcRetireIncome(balance), 0);
+            this.monthlyRetireIncome = FormatUtil.formatNumber(calcRetireIncome(balance) / 12, 0);
             this.maxX = i;
             break;
           }
+          expensePerYear += calcGrowth(expensePerYear, inflationRate);
+          targetPrincipal = calcFireNumber(expensePerYear)
         }
       }
 
@@ -254,27 +314,55 @@ export class WhenToRetireComponent extends BaseComponent implements OnInit {
         this.minY = 0;
       }
 
-      return [{
-        data: balances,
-        label: "Balance"
-      },{
-        data: contributions,
-        label: "Contributions"
-      }];
-  }
+      if (!this.showAsset) {
+        return [{
+          data: incomes,
+          label: "Income"
+        },{
+          data: expenses,
+          label: "Expenses"
+        }];
+      }
+      else {
+        return [{
+          data: balances,
+          label: "Balance"
+        },{
+          data: contributions,
+          label: "Contributions"
+        }];
+      }
+    }
+  
+  toggleChart() { 
+      this.showAsset = !this.showAsset ;
+      this.onChange();
+    }
 
   generateAndShowTable() {
     
     this.tableDataRows = [];
     this.showTable = !this.showTable ;
-    if (this.showTable) {
+
+    if (!this.showTable) return;
+
+    if (!this.showAsset) {
+      for(let i=0; i < this.lineChartData.labels!.length; i++) {
+        this.tableDataRows.push(new TableDataRow(
+          this.lineChartData.labels![i] as string,
+          FormatUtil.formatNumber(this.lineChartData.datasets[0].data[i] as number),
+          FormatUtil.formatNumber(this.lineChartData.datasets[1].data[i] as number),
+        ))
+      }
+    }
+    else {
       for(let i=0; i < this.barChartData.labels!.length; i++) {
         this.tableDataRows.push(new TableDataRow(
           this.barChartData.labels![i] as string,
           FormatUtil.formatNumber(this.barChartData.datasets[0].data[i] as number),
-          FormatUtil.formatNumber(this.barChartData.datasets[1].data[i] as number)
+          FormatUtil.formatNumber(this.barChartData.datasets[1].data[i] as number),
         ))
-      }
+      }     
     }
   }
 }
